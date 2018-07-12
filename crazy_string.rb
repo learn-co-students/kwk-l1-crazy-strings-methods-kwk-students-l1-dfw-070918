de5aa52a422e2a83c08e9d032e6650c4e3b47b96
Chamberlain Churchill <cbchurchill2004@icloud.com>
2018-07-12 21:34:37 +0000

    Done.

--- crazy_string.rb
+++ crazy_string.rb
@@ -1,5 +1,9 @@
 # write your method here
 
-def crazy_strings(a,b)
-
+def crazy_strings(a, b)
+  a = a.reverse.upcase
+  b = b.swapcase.gsub("s", "z").gsub("S", "Z")
 end
+
+puts crazy_strings("ChaCha", "Chamberlain")
+puts crazy_strings("Janice", "Amanda")
